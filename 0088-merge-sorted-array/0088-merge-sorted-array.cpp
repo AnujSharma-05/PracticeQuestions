69cd02c5438c29e9f832ec40ff133e0ebc9e0d8e
Anuj Sharma <139798936+AnujSharma-05@users.noreply.github.com>
2026-07-27 00:42:08 +0530

    Time: 0 ms (100%), Space: 12.5 MB (7.27%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    // ------------Naive Approach Using extra vector Temp-------------
+    // ------------Approach without Using extra vector Temp-------------
+    //We are using 3 pointers and comparisng the 2 vectors backwards hence storing the extra pieces in the unused space of the nums1 taking the advantage of the sorted property
+    // time complexity is O(n+m) and space complexity is 0;
+
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        vector<int> temp;
-        int ptr1 = 0 , ptr2 = 0;
-        for(int i=0; i<(m+n) ; i++){
-            if(ptr2 < n && nums1[ptr1] <= nums2[ptr2] && ptr1<m){
-                temp.push_back(nums1[ptr1]);
-                ptr1++;
-            }
-            else if(ptr2 < n && nums1[ptr1]>nums2[ptr2]){
-                temp.push_back(nums2[ptr2]);
-                ptr2++;
-            }
-            else if(ptr1>=m && ptr2 < n){
-                temp.push_back(nums2[ptr2]);
-                ptr2++;
+        int ptr1 = m-1 , ptr2 = n-1;
+        for(int p = m+n-1; p>=0 ; p--){
+            if(ptr2<0) break; //cause then the nums1 is already in order
+            if(ptr1>=0 && nums1[ptr1]>nums2[ptr2]){
+                nums1[p] = nums1[ptr1];
+                ptr1--;
             }
             else{
-                temp.push_back(nums1[ptr1]);
-                ptr1++;
+                nums1[p] = nums2[ptr2];
+                ptr2--;
             }
         }
-        nums1 = temp;
+        
     }
 };
